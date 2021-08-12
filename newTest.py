c7af32e262d57ff29295eabe276fa83d5a0e66a4
ParkSeJun <fgfg1202@gmail.com>
2021-08-12 23:30:33 +0900

    no message

--- newTest.py
+++ newTest.py
@@ -1,25 +1,36 @@
 import sys
 import os
-import datetime
 
 if __name__ == "__main__" :
     print("START")
 
     mainSource = sys.argv[1]
-    firstSha = sys.argv[2]
-    secondSha = sys.argv[len(sys.argv)-1]
-
-    for index, arg in enumerate(sys.argv): 
-        print('index = {0}, arg value = {1}' .format(index, arg))
-
-    print("결과값")
-    print(mainSource)
-    print(firstSha)
-    print(secondSha)
+    lastSHA = sys.argv[2]
+    startSHA = sys.argv[len(sys.argv)-1]
+    mainSource= mainSource.replace("\\" , os.sep)
 
     os.system('cd {}'.format(mainSource))
-    output = os.popen('git diff --name-only {0} {1}'.format(firstSha, secondSha)).read()
-    print(output)
+    output = os.popen('git diff --name-only {0} {1}'.format(startSHA, lastSHA)).read()
+    outputList = output.splitlines()
+
+    copySource = os.path.join('C:',  os.sep,'CopyFiles')
+    os.makedirs(copySource, exist_ok=True)
+
+    for index, mkFile in enumerate(outputList):
+        copySource = os.path.join('C:',  os.sep,'CopyFiles')
+        chkList = mkFile.split("/")
+        for chk in chkList[:-1]:
+            copySource = copySource + os.sep + chk
+       
+        mkFile= mkFile.replace("/", os.sep)
+        originPath = mainSource +  os.sep + mkFile
 
 
-os.system('pause')
+        os.makedirs(copySource, exist_ok=True)
+        os.popen('copy {0} {1}'.format(originPath, copySource))
+
+        print("{0} | {1} : {2} ".format(index, len(outputList), originPath))
+
+
+copySource = os.path.join('C:',  os.sep,'CopyFiles')
+os.startfile(copySource)
