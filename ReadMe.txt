fdb334a41347698b5860a7dc2d7593adece004ed
ParkSeJun <fgfg1202@gmail.com>
2021-08-12 23:44:33 +0900

    no message

---
+++ ReadMe.txt
@@ -0,0 +1,10 @@
+
+pip install pyinstaller 설치
+
+pyinstaller --onefile newTest.py
+
+파일을 exe 로 만들어줌
+
+소스트리 연결
+
+파일 복사가 정상적으로 되는지 확인
--- newTest.py
+++ newTest.py
@@ -1,36 +1,44 @@
 import sys
 import os
 
-if __name__ == "__main__" :
-    print("START")
-
-    mainSource = sys.argv[1]
-    lastSHA = sys.argv[2]
-    startSHA = sys.argv[len(sys.argv)-1]
-    mainSource= mainSource.replace("\\" , os.sep)
-
-    os.system('cd {}'.format(mainSource))
-    output = os.popen('git diff --name-only {0} {1}'.format(startSHA, lastSHA)).read()
-    outputList = output.splitlines()
+def copyFileMethod(outputList):
 
     copySource = os.path.join('C:',  os.sep,'CopyFiles')
     os.makedirs(copySource, exist_ok=True)
 
     for index, mkFile in enumerate(outputList):
+
         copySource = os.path.join('C:',  os.sep,'CopyFiles')
         chkList = mkFile.split("/")
+
         for chk in chkList[:-1]:
             copySource = copySource + os.sep + chk
        
         mkFile= mkFile.replace("/", os.sep)
         originPath = mainSource +  os.sep + mkFile
 
-
         os.makedirs(copySource, exist_ok=True)
         os.popen('copy {0} {1}'.format(originPath, copySource))
 
-        print("{0} | {1} : {2} ".format(index, len(outputList), originPath))
+        print("{0:03d}|{1:03d} : {2} ".format(index, len(outputList), originPath))
 
 
+if __name__ == "__main__" :
+
+    print("SourceTree Copy Start")
+
+    mainSource = sys.argv[1]
+    lastSHA = sys.argv[2]
+    startSHA = sys.argv[len(sys.argv)-1]
+
+    mainSource= mainSource.replace("\\" , os.sep)
+    os.system('cd {}'.format(mainSource))
+
+    output = os.popen('git diff --name-only {0} {1}'.format(startSHA, lastSHA)).read()
+
+    copyFileMethod(output.splitlines())
+    
 copySource = os.path.join('C:',  os.sep,'CopyFiles')
+
+os.system('pause')
 os.startfile(copySource)
