c1862a63467474f58c37c214a31fea2eb9de5bd1
ParkSeJun <fgfg1202@gmail.com>
2021-08-12 21:40:33 +0900

    FIRST

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
+import sys
 import os
 
+print("START")
+print("START")
+print("START")
+print(sys.argv[0])
+print(sys.argv[1])
+print(sys.argv[2])
+print(sys.argv[3])
 
-print("Hello WORLD")
+os.system('pause')
