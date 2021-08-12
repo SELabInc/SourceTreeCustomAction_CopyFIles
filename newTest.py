39a1de6c39f83d7fe8c8750fa2b90418aa56c681
ParkSeJun <fgfg1202@gmail.com>
2021-08-12 22:12:13 +0900

    no message

--- newTest.py
+++ newTest.py
@@ -3,10 +3,23 @@ import os
 import datetime
 
 if __name__ == "__main__" :
-    print(sys.argv[0])
-    print(sys.argv[1])
-    print(sys.argv[2])
-    print(sys.argv[3])
+    print("START")
+
+    mainSource = sys.argv[1]
+    firstSha = sys.argv[2]
+    secondSha = sys.argv[len(sys.argv)-1]
+
+    for index, arg in enumerate(sys.argv): 
+        print('index = {0}, arg value = {1}' .format(index, arg))
+
+    print("결과값")
+    print(mainSource)
+    print(firstSha)
+    print(secondSha)
+
+    os.system('cd {}'.format(mainSource))
+    output = os.popen('git diff --name-only {0} {1}'.format(firstSha, secondSha)).read()
+    print(output)
 
 
 os.system('pause')
