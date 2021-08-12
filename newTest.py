29810c5be0b23c18eb555844f87e9bc91f2c098a
ParkSeJun <fgfg1202@gmail.com>
2021-08-12 21:40:46 +0900

    TREEE

---
+++ newTest.py
@@ -0,0 +1,12 @@
+import sys
+import os
+import datetime
+
+if __name__ == "__main__" :
+    print(sys.argv[0])
+    print(sys.argv[1])
+    print(sys.argv[2])
+    print(sys.argv[3])
+
+
+os.system('pause')
