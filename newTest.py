17b18b126342f050b45aec62f8066f85c4f08b28
ParkSeJun <fgfg1202@gmail.com>
2021-08-13 00:27:25 +0900

    진짜 최종

--- newTest.py
+++ newTest.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import shutil
 
 def copyFileMethod(outputList):
 
@@ -8,19 +9,27 @@ def copyFileMethod(outputList):
 
     for index, mkFile in enumerate(outputList):
 
-        copySource = os.path.join('C:',  os.sep,'CopyFiles')
-        chkList = mkFile.split("/")
-
-        for chk in chkList[:-1]:
-            copySource = copySource + os.sep + chk
-       
-        mkFile= mkFile.replace("/", os.sep)
         originPath = mainSource +  os.sep + mkFile
 
-        os.makedirs(copySource, exist_ok=True)
-        os.popen('copy {0} {1}'.format(originPath, copySource))
-        results = round((index+1) /len(outputList), 2 ) * 100
-        print("{0:>6.2f}% - {1}".format(results, originPath))
+        if os.path.isfile(originPath):
+
+            copySource = os.path.join('C:',  os.sep,'CopyFiles')
+            chkList = mkFile.split("/")
+
+            for chk in chkList[:-1]:
+                copySource = copySource + os.sep + chk
+        
+            mkFile= mkFile.replace("/", os.sep)
+            os.makedirs(copySource, exist_ok=True)
+
+            try:
+                # os.popen('copy {0} {1}'.format(originPath, copySource))
+                shutil.copy(originPath, copySource)
+            except BaseException as e:
+                print(e)
+
+            results = round((index+1) /len(outputList), 2 ) * 100
+            print("{0:>6.2f}% - {1}".format(results, originPath))
 
 
 if __name__ == "__main__" :
@@ -38,7 +47,8 @@ if __name__ == "__main__" :
 
     copyFileMethod(output.splitlines())
     
-copySource = os.path.join('C:',  os.sep,'CopyFiles')
+print("작업이 완료되었습니다!")
 
+copySource = os.path.join('C:',  os.sep,'CopyFiles')
 os.system('pause')
 os.startfile(copySource)
