9c187b1385175ea46974f1236357e1e2e2376b03
박세준 <sjpark@selab.co.kr>
2021-08-24 18:22:43 +0900

    FEA: 정렬 기능 추가 / 원하는 확장자만 추가할수 있게 추가 사용자 입력받게 추가 인터페이스 업데이트

--- newTest.py
+++ newTest.py
@@ -2,10 +2,22 @@ import sys
 import os
 import shutil
 
+
+typeValueDic = {}
 logList =[]
+filteredOutputList =[]
+
+def getKey(selector):
+    for key, dicValue in typeValueDic.items():
+         if selector == dicValue:
+             return key
+ 
+    return "There is no such Key"
+
 
 def copyEachFile(outputList, mainSource):
-   
+    print("=========================================================")
+    print("Copy Start")
     for index, mkFile in enumerate(outputList):
         originPath = os.sep.join([mainSource, mkFile]) 
 
@@ -44,16 +56,62 @@ def copyFileMethod(outputList, mainSource):
 
     f.close()
 
-if __name__ == "__main__" :
+def filteringList(outputList, typeList, typeValueDic):
+    selector = 99
+    while selector > -1:
+        print("=========================================================")
+        print("복사하지 않을 확장자 번호를 선택하세요")
+        print("취소하려면 0번을 선택해주세요.")
+        for turples in  typeValueDic.items():
+            print("{0} : {1}".format(turples[1] + 1, turples[0]))
+        try:
+            inputData = sys.stdin.readline()
+            selector = int(inputData) - 1
+        except Exception as e:
+            print(e)
+            if inputData == '\n':
+                selector = -1
 
+        tempnum = getKey(selector)
+
+        if tempnum != "There is no such Key":
+            del typeValueDic[tempnum]
+            print("=========================================================")
+            print("{0} 확장자 제외 완료 ".format(tempnum))
+
+
+    for filteredTurple in typeValueDic.items():
+        for filtering in typeList[filteredTurple[1]]:
+            filteredOutputList.append(outputList[filtering])
+
+
+if __name__ == "__main__" :
+    print("=========================================================")
     print("SourceTree Copy Start")
+    output = os.popen('git diff --name-only --diff-filter=d {0} {1}'.format(sys.argv[len(sys.argv)-1], sys.argv[2])).read()
+    dicIndex = 0
+    typeList = []
+    outputList = output.splitlines()
+    for idx,line in enumerate(outputList):
+        
+        while line.find(".") > -1:
+            line = line[line.find(".") + 1:]
+
+        if line not in typeValueDic:
+            typeList.append([])
+            typeValueDic[line] = dicIndex
+            dicIndex += 1
+
+        typeList[typeValueDic[line]].append(idx)
+
+    filteringList(outputList, typeList, typeValueDic)
+    
     mainSource= sys.argv[1].replace("\\" , os.sep)
     os.chdir(mainSource)
-    alloutput = os.popen('git diff --name-only {0} {1}'.format(sys.argv[len(sys.argv)-1], sys.argv[2])).read()
-    output = os.popen('git diff --name-only --diff-filter=d {0} {1}'.format(sys.argv[len(sys.argv)-1], sys.argv[2])).read()
-    copyFileMethod(output.splitlines(), mainSource)
-    
 
+    copyFileMethod(filteredOutputList, mainSource)
+    
+print("=========================================================")
 print("Copy Finished!\nPlease check the logFile of the copied directory.")
 os.system('pause')
 
